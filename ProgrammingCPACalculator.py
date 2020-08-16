99a7896bc974512138b38c2f1c48a17a6257d0c2
MUPAJELINGAU <69494595+mupajelingau@users.noreply.github.com>
2020-08-16 12:55:28 +0800

    Update ProgrammingCPACalculator.py
    i have add print statement

--- ProgrammingCPACalculator.py
+++ ProgrammingCPACalculator.py
@@ -1,1 +1,13 @@
 print("Attention Everybody")
+
+Total_Time = 73
+Credit_Hour = 4
+Gred_A = 3.75>4.00
+Gred_B = 3.50>3.74
+Gred_C = 3.00>3.49
+Gred_D = 2.00>2.99
+Gred_F = 0.00>1.99
+
+
+print (3.80*4+3.74*4+3.54+3.80*4+3.74+3.54*4/Total_Time)
+
